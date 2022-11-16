81552ae83bee4e9dd11069313a1e00f0b58f3814
Solti Peter <solti.peti00@gmail.com>
2022-11-16 14:39:50 +0100

    Models letrehozva

---
+++ YL1CC3_HFT_2022231.Models/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YL1CC3_HFT_2022231.Models
+{
+    class Car
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+        [NotMapped]
+        public virtual Brand Brand { get; set; }
+        public int BrandId { get; set; }
+    }
+}
---
+++ YL1CC3_HFT_2022231.Models/Rent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YL1CC3_HFT_2022231.Models
+{
+    class Rent
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [NotMapped]
+        public virtual Car Car { get; set; }
+
+    }
+}
