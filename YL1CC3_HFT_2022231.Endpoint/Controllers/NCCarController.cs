c56f1ba084f6be9440f535a2764b09380ed64f2f
Solti Peter <solti.peti00@gmail.com>
2022-11-19 12:13:00 +0100

    Keson lett ebed :(

---
+++ YL1CC3_HFT_2022231.Endpoint/Controllers/NCCarController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YL1CC3_HFT_2022231.Logic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace YL1CC3_HFT_2022231.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NCCarController : ControllerBase
+    {
+        ICarLogic logic;
+
+        public NCCarController(ICarLogic logic)
+        {
+            this.logic = logic;
+        }
+        [HttpGet]
+        public IEnumerable<RentFrequency> FreqOfCarsRented()
+        {
+            return this.logic.FreqOfCarsRented();
+        }
+    }
+}
